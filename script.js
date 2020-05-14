732d255b073d6016713f71d1cf112838670b851b
Thalles Sobral <thazsobral@gmail.com>
2020-05-14 09:01:14 -0300

    adicionei um novo script.js com o código mais enxuto

--- script.js
+++ script.js
@@ -1,40 +1,11 @@
-function getDateOfBirth () {
-  return new Date(document.getElementById('date-of-birth').value)
-}
-
-function getDateToday () {
-  return new Date()
-}
-
-function getDiferenceDays (initialDate, finalDate) {
-  return Math.abs(finalDate.getDate() - initialDate.getDate())
-}
-
-function getDiferenceMonths (initialDate, finalDate) {
-  return Math.abs(finalDate.getMonth() - initialDate.getMonth() )
-}
-
-function getDiferenceYears (initialDate, finalDate) {
-  return Math.abs(finalDate.getFullYear() - initialDate.getFullYear())
-}
-
-function getDiferenceDate (initialDate, finalDate) {
-  let days = getDiferenceDays(initialDate, finalDate)
-  let months = getDiferenceMonths(initialDate, finalDate)
-  let years = getDiferenceYears(initialDate, finalDate)
-
-  return { days, months, years}
-}
-
-function showDiferenceDate (date) {
-  document.getElementById('board').innerText = `Dias de vida: ${date.days}\nMeses de vida: ${date.months}\nAnos de vida: ${date.years}`
-}
-
 function calculateLifetime () {
-  var dateOfBirth = getDateOfBirth()
+  const now = new Date()
+  const dateOfBirth = new Date(document.getElementById('date-of-birth').value)
+  const diference = Math.abs(now.getTime() - dateOfBirth.getTime())
+  const days = Math.ceil(diference / (1000 * 60 * 60 *24))
+
+  document.getElementById('board').innerText = `Ou seja:\nEm dias: ${days}\nEm meses aprox.: ${Math.floor(days/30)}\nEm anos aprox.: ${Math.floor(days/(30*12))} `
+  console.log(days%(30*12))
+  console.log((days%(30*12))%30);
   
-  var dateToday = getDateToday()
-  console.log(dateOfBirth)
-  var diferenceDate = getDiferenceDate(dateOfBirth, dateToday)
-  showDiferenceDate(diferenceDate)
 }
